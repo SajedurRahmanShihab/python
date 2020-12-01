8b360a2943f896f538ece9007162cfc4989ada51
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-02 01:33:20 +0600

    final commit

---
+++ DataType.py
@@ -0,0 +1,8 @@
+# Built in data type
+# Text Type: str
+# Numeric Type: int, float, complex
+# Sequence Types: list, tuple, range
+# Mapping Types: dict
+# Set Types: set, frozenset
+# Boolean Type: bool
+# Binary Types: bytes, bytearray, memoryview
