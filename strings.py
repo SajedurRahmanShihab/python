d8a0a38dc72edd6af75a8c57bd211452921da83e
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-02 02:22:50 +0600

    completed from w3school

--- strings.py
+++ strings.py
@@ -69,3 +69,24 @@ print(a.replace("H", "J"))
 # Split String
 a = "Hello, World!"
 print(a.split(","))
+
+# Format Strings
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+# Escape Character
+txt = "We are the so called \"Vikings\" from the north."
+print(txt)
