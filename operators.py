a625f2cc5e379eba26f8dd24cfa50a414451928f
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-02 02:30:36 +0600

    all operators

---
+++ operators.py
@@ -0,0 +1,52 @@
+# Python Arithmetic Operators
++
+-
+*
+/
+%
+** (Exponentiation)
+// (Floor division)
+
+# Python Assignment Operators
+=
++=
+-=
+*=
+/=
+%=
+//=
+**=
+&=
+|=
+^=
+>>=
+<<=
+
+# Python Comparison Operators
+==
+!=
+>
+<
+>=
+<=
+
+# Python Logical Operators
+and
+or
+not
+
+# Python Identity Operators
+is
+is not
+
+# Python Membership Operators
+in
+not in
+
+# Python Bitwise Operators
+&
+|
+^
+~
+<<
+>>
