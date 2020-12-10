e2979534709ab3471e71676d79faa13a887f5d7b
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-10 10:36:22 +0600

    done

--- forLoop.py
+++ forLoop.py
@@ -35,3 +35,16 @@ fruits = ["apple", "banana", "cherry"]
 for x in adj:
     for y in fruits:
         print(x, y)
+
+
+def tri_recursion(k):
+    if(k > 0):
+        result = k = tri_recursion(k-1)
+        print(result)
+    else:
+        result = 0
+    return result
+
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
