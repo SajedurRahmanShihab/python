526430f619600947b943aa38c3359b9f37c23663
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-15 01:16:47 +0600

    done

---
+++ Modules.py
@@ -0,0 +1,24 @@
+# import test
+# test.greeting("Sajedur")
+
+# Variable in Module
+# import platform
+# import test as t
+# import test
+# a = test.person1["age"]
+# print(a)
+
+# b = t.person1["country"]
+# print(b)
+
+# Built-in Modules
+# x = platform.system()
+# print(x)
+
+# Using the dir() Function
+# x = dir(platform)
+# print(x)
+
+# Import From Module
+from test import person1
+print(person1["age"])
